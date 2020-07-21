8022ee5a78172923e76fe209d062616371ed932b
Anibal Lovaglio <1944035+elcoya@users.noreply.github.com>
2020-07-21 15:49:31 -0300

    Initial commit - first solution for the problem

---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(System.in);
+        String line = null;
+
+        int lines = 0;
+        int counter = 0;
+        PrintStream out = new PrintStream(String.format("out-%03d.txt", counter++));
+
+        if (in.hasNextLine()) {
+            line = in.nextLine();
+        } else {
+            line = null;
+        }
+
+        while (! (line == null || "".equalsIgnoreCase(line)) ) {
+            if(lines > 4) {
+                out.close();
+                out = new PrintStream(String.format("out-%03d.txt", counter++));
+                lines = 0;
+            }
+            out.println(line);
+            lines++;
+            if (in.hasNextLine()) {
+                line = in.nextLine();
+            } else {
+                line = null;
+            }
+        }
+        out.close();
+        System.out.println(String.format("Process completed. [%d] files were created.", counter));
+
+        File dir = new File(".");
+        File[] filesList = dir.listFiles();
+        for (File file : filesList) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+            }
+        }
+        in = new Scanner(new FileInputStream("out-000.txt"));
+        while (in.hasNextLine()) {
+            System.out.println(in.nextLine());
+        }
+        in.close();
+
+    }
+
+
+}
